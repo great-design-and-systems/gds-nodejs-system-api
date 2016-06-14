821fbee5f15ee18dde8f9307c4396f804f5ca810
Jerico de Guzman <rickzx98@gmail.com>
2016-06-15 02:49:22 +0800

    updates

--- src/config/server.ts
+++ src/config/server.ts
@@ -3,6 +3,7 @@ import morgan = require("morgan");
 const PORT = process.env.port || 3000;
 
 export function Server(app) {
+    app.set('port', (process.env.PORT || 5000));
     app.use(morgan("dev"));
     app.use(bodyParser.urlencoded({
         extended: true
@@ -11,8 +12,8 @@ export function Server(app) {
     app.use(bodyParser.json({
         type: "application/vnd.api+json"
     }));
-    app.listen(PORT, () => {
-        console.log("listening on PORT" + PORT);
+    app.listen(app.get('port'), () => {
+        console.log('Node app is running on port', app.get('port'));
     });
     app.get("/", (req, res) => res.send("Welcome to GDS System API"));
 }
